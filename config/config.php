6c670cb82062f74bd13d287100b9a7d35e9df6e0
jopmesman <jomesie@MacBook-Pro-van-Jh.local>
2013-10-11 14:07:52 +0200

    new commit

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+//The absolute path to the index.php
+define('SERVER_ROOT', '/volume1/web/jframe./');
+//The site root
+define('SITE_ROOT', 'http://192.168.1.60/jframe//');
+define('BASE_PATH', '/jframe/');
+//Waths the name of the main template (without the .php suffix
+define('MAIN_TEMPLATE', 'main');
+
+//Database Host
+define('DB_HOST', 'localhost');
+//Database username
+define("DB_USERNAME", 'root');
+//Database password
+define('DB_PASSWORD', 'root');
+//Database name
+define('DB_DATABASE', 'jframe');
+
+//this creates an array key in the session. There will be no clashes with other session data
+define('SESSNAME', 'xx');
