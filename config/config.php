d024998b6190005aae7905c8599cc6846ec83e1c
jopmesman <jomesie@MacBook-Pro-van-Jh.local>
2013-10-15 09:27:02 +0200

    added won_config

--- config/config.php
+++ config/config.php
@@ -1,29 +1,29 @@
 <?php
 
-if (file_exists('own_config.php')) {
+if (file_exists('config/own_config.php')) {
   /**
    * Is this file you can override the config file.
    * Add items of the config here
    * Just add the define.
-   * 
+   *
    */
   require_once 'own_config.php';
 }
 
 //The absolute path to the index.php
-if (!defined('SERVER_ROOT')) define('SERVER_ROOT', '/volume1/web/jframe/');
+if (!defined('SERVER_ROOT')) define('SERVER_ROOT', '/web/httpdocs/');
 //The site root
-if (!defined('SITE_ROOT')) define('SITE_ROOT', 'http://192.168.1.60/jframe//');
-if (!defined('BASE_PATH')) define('BASE_PATH', '/jframe/');
+if (!defined('SITE_ROOT')) define('SITE_ROOT', 'http://www.example.com/');
+if (!defined('BASE_PATH')) define('BASE_PATH', '/');
 //Waths the name of the main template (without the .php suffix
 if (!defined('MAIN_TEMPLATE')) define('MAIN_TEMPLATE', 'main');
 
 //Database Host
 if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
 //Database username
-if (!defined('DB_USERNAME')) define("DB_USERNAME", 'root');
+if (!defined('DB_USERNAME')) define("DB_USERNAME", 'username');
 //Database password
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', 'root');
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', 'password');
 //Database name
 if (!defined('DB_DATABASE')) define('DB_DATABASE', 'jframe');
 //What is the driver of the database?
